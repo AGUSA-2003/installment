931043ac6e615db9315a35843a71e2f3514557ef
unknown <aghaghadirialex@gmail.com>
2024-08-10 18:20:03 +0330

    test word

---
+++ installment.php
@@ -0,0 +1,36 @@
+<?php
+/*
+Plugin Name: پلاگین اقساطی محصولات اپل
+Plugin URI: http://installment.com
+Description: مدیریت محصولات اقساطی اپل
+Author: Alex Aghaghadiri Kashi
+Version: 1.0.0
+Author URI: http://github.com/AGUSA-2003
+*/
+
+
+add_action('admin_menu', function (){
+	add_menu_page(
+		'اقساط',
+		'محصولات',
+	'manage_options',
+
+	
+	'installment', function () {
+	echo "hello";
+	});
+
+add_submenu_page(
+	'installment',
+	'اضافه کردن محصولات',
+	'اضافه کردن محصولات',
+	'manage_options',
+	'add_installment', function(){
+
+		echo "<h1>welcome to panel</h1>";
+	}
+);
+
+
+
+});
